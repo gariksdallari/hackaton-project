c5f9a5347ad50ed9c05ba037e733285f984e7ddf
gariks.dallari <gariks.dallari@dynamicyield.com>
2023-04-21 11:35:03 +0300

    make nav

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<Navigation/>}>
-        <Route index/>
+        <Route index />
         <Route path='/shop' />
         <Route path='/checkout' />
         <Route path='/auth' />
--- src/Routes/Navigation/Navigation.component.js
+++ src/Routes/Navigation/Navigation.component.js
@@ -1,13 +1,14 @@
 import { Link, Outlet } from "react-router-dom";
+import  classes from './Navigation.module.css';
 
 const Navigation = () => {
     return (
-        <nav>
-           <Link to='/home'>HOME</Link>
-           <Link to='/shop'>SHOP</Link>
-           <Link to='/checkout'>CHECKOUT</Link>
-           <Link to='/auth'>AUTH</Link>
-           <Link to='/contactus'>CONTACT US</Link>
+        <nav className={classes.container}>
+           <Link className={classes.item} to='/home'>HOME</Link>
+           <Link className={classes.item} to='/shop'>SHOP</Link>
+           <Link className={classes.item} to='/checkout'>CHECKOUT</Link>
+           <Link className={classes.item} to='/auth'>AUTH</Link>
+           <Link className={classes.item} to='/contactus'>CONTACT US</Link>
            <Outlet/>
         </nav>
     )
