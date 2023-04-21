8de45b8ed496d526d3740bcf4753208de279d186
gariks.dallari <gariks.dallari@dynamicyield.com>
2023-04-21 12:15:49 +0300

    shop component

--- src/Routes/Home/Home.component.js
+++ src/Routes/Home/Home.component.js
@@ -1,5 +1,13 @@
+import classes from "./Home.module.css";
+
 const Home = () => {
-    return <div>home</div>
+  return (
+    <div>
+      <div className={classes.hero}>
+        <img alt=''/>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
