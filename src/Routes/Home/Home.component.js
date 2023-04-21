9c2178c130d88e84451d41886307480c4249a08c
gariks.dallari <gariks.dallari@dynamicyield.com>
2023-04-21 12:54:58 +0300

    homepage

--- src/Routes/Home/Home.component.js
+++ src/Routes/Home/Home.component.js
@@ -1,10 +1,21 @@
 import classes from "./Home.module.css";
+import hero from "../../assets/logos/f5bf51a5-1ecf-4651-a5ee-e8afe3a091a3.jpeg";
+import { BsInstagram, BsFacebook } from "react-icons/bs";
 
 const Home = () => {
   return (
-    <div>
+    <div className={classes.container}>
       <div className={classes.hero}>
-        <img alt=''/>
+        <img className={classes.heroImage} src={hero} alt="" />
+        <p>
+          Welcome to SMURF Shop! Here you can find everything what you want. 😎
+        </p>
+        <br />
+        <p>Have a good Time! ❤️</p>
+      </div>
+      <div className={classes.footer}>
+        <a style={{marginRight: '20px'}} href='/'><BsInstagram color="black" size={50}/></a>
+        <a href='/'><BsFacebook color="black" size={50}/></a>
       </div>
     </div>
   );
