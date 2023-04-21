059cde98239e67102ae2b9416c888351806d4fe1
gariks.dallari <gariks.dallari@dynamicyield.com>
2023-04-21 11:53:15 +0300

    finish nav

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
-import Navigation from './Routes/Navigation/Navigation.component';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./Routes/Navigation/Navigation.component";
+import "./App.css";
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Navigation/>}>
+      <Route path="/" element={<Navigation />}>
         <Route index />
-        <Route path='/shop' />
-        <Route path='/checkout' />
-        <Route path='/auth' />
-        <Route path='/contactus' />
+        <Route path="/shop" />
+        <Route path="/checkout" />
+        <Route path="/auth" />
+        <Route path="/contactus" />
       </Route>
     </Routes>
   );
